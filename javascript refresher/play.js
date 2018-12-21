e3e9a07a6f9c14ae58de87111589d2ff8fb4703c
yosoydead <radu-bogdan@outlook.com>
2018-12-21 09:53:53 +0200

    arrow functions

--- javascript refresher/play.js
+++ javascript refresher/play.js
@@ -27,6 +27,24 @@ const summ = function (userName, userAge, hobbies){
 }
 
 //arrow functions
+const summ2 = (userName, userAge, hobbies) =>{
+    return ("Name is " + userName + 
+    " , age is " + userAge + 
+    " and has hobbies " + hobbies);
+}
+
+const add = (a,b)=>{
+    return a+b;
+}
+// you can ommit using the return keyword
+const add2 = (a,b) => a+b;
+
+//if you have only one argument, you can ommit the () around the argument
+const addOne = a => a+1;
+//arrow function without arguments
+const addRandom = ()=> 1 + 2;
 
 //console.log(summ(name, age, hasHobbies));
+console.log(add2(2,3));
+//console.log(addOne(3));
 console.log(summarize(name,age,hasHobbies));
