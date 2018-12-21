8a3977163962d58a0d570a71af564cc0ccabdb80
yosoydead <radu-bogdan@outlook.com>
2018-12-21 09:45:54 +0200

    used anonymous function

--- javascript refresher/play.js
+++ javascript refresher/play.js
@@ -18,4 +18,15 @@ function summarize(userName, userAge, hobbies){
     " and has hobbies " + hobbies;
 }
 
+//anonymous function
+//does the same thing like the above one
+const summ = function (userName, userAge, hobbies){
+    return "Name is " + userName + 
+    " , age is " + userAge + 
+    " and has hobbies " + hobbies;
+}
+
+//arrow functions
+
+//console.log(summ(name, age, hasHobbies));
 console.log(summarize(name,age,hasHobbies));
