525bf25180878a264aeddb04c12aa27a99c192fd
yosoydead <radu-bogdan@outlook.com>
2018-12-27 15:37:55 +0200

    updating the cart and deleting items from the cart

--- dynamic routes and advanced models/controllers/admin.js
+++ dynamic routes and advanced models/controllers/admin.js
@@ -37,18 +37,21 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-exports.postEditProduct = (req,res,next) => {
+exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  const updatedTitle= req.body.title;
+  const updatedTitle = req.body.title;
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
-  const updatedDescription = req.body.description;
-
-  const newProduct = new Product(prodId, updatedTitle,updatedImageUrl,updatedDescription,updatedPrice);
-
-  newProduct.save();
-
-  res.redirect("/admin/products");
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
@@ -59,4 +62,10 @@ exports.getProducts = (req, res, next) => {
       path: '/admin/products'
     });
   });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
 };
