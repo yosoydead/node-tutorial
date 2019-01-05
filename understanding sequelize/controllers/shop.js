806c7a22bc47fa2f6d961442cd13868c23efa016
yosoydead <radu-bogdan@outlook.com>
2019-01-05 17:33:30 +0200

    deleting items from the cart

--- understanding sequelize/controllers/shop.js
+++ understanding sequelize/controllers/shop.js
@@ -178,10 +178,24 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
+  // Product.findById(prodId, product => {
+  //   Cart.deleteProduct(prodId, product.price);
+  //   res.redirect('/cart');
+  // });
+
+  req.user.getCart()
+    .then(cart =>{
+      return cart.getProducts( {where: {id: prodId} });
+    })
+    .then(products => {
+      //after i retrieve the cart, i store the products from it
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(error => console.log(error));
 };
 
 exports.getOrders = (req, res, next) => {
